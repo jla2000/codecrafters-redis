45b2e46b4f0f208a7c9f9a9eeae6bad64e480aec
Jan Lafferton <jan@lafferton.de>
2025-08-03 00:11:09 +0200

    rpush

--- src/main.rs
+++ src/main.rs
@@ -23,12 +23,18 @@ use nom::{
 use nom::{bytes::tag, character::complete::char, sequence::delimited};
 use strum::EnumString;
 
+#[derive(Default)]
+struct Database {
+    values: HashMap<String, (String, Option<SystemTime>)>,
+    lists: HashMap<String, Vec<String>>,
+}
+
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    let db = Mutex::new(HashMap::new());
+    let db = Mutex::new(Database::default());
 
     std::thread::scope(|s| {
         for stream in listener.incoming() {
@@ -45,7 +51,7 @@ fn main() {
     })
 }
 
-fn handle_client(mut stream: TcpStream, db: &Mutex<HashMap<String, (String, Option<SystemTime>)>>) {
+fn handle_client(mut stream: TcpStream, db: &Mutex<Database>) {
     let mut buf = [0; 512];
 
     while stream.read(&mut buf).unwrap() > 0 {
@@ -56,10 +62,20 @@ fn handle_client(mut stream: TcpStream, db: &Mutex<HashMap<String, (String, Opti
         let response = match data_parts.next().unwrap().to_ascii_uppercase().as_str() {
             "PING" => build_simple_string("PONG"),
             "ECHO" => build_bulk_string(data_parts.next().unwrap()),
+            "RPUSH" => {
+                let key = data_parts.next().unwrap();
+                let value = data_parts.next().unwrap();
+
+                let mut db = db.lock().unwrap();
+                let list = db.lists.entry(key.into()).or_default();
+                list.push(value.into());
+
+                build_integer(list.len())
+            }
             "GET" => {
                 let key = data_parts.next().unwrap();
 
-                match db.lock().unwrap().get(key) {
+                match db.lock().unwrap().values.get(key) {
                     Some((value, None)) => build_bulk_string(value),
                     Some((value, Some(expiry))) if SystemTime::now() < *expiry => {
                         build_bulk_string(value)
@@ -86,6 +102,7 @@ fn handle_client(mut stream: TcpStream, db: &Mutex<HashMap<String, (String, Opti
                 assert!(db
                     .lock()
                     .unwrap()
+                    .values
                     .insert(key.to_string(), (value.to_string(), expiry))
                     .is_none());
 
@@ -108,18 +125,14 @@ fn build_simple_string(data: &str) -> String {
     format!("+{data}\r\n")
 }
 
+fn build_integer(value: usize) -> String {
+    format!(":{value}\r\n")
+}
+
 fn parse_array(input: &[u8]) -> IResult<&[u8], Vec<&str>> {
     let (input, num_elements) = delimited(char('*'), parse_number, tag("\r\n")).parse(input)?;
     many(0..=num_elements, parse_bulk_string).parse(input)
 }
-
-// fn parse_simple_string(input: &[u8]) -> IResult<&[u8], &str> {
-//     map_res(
-//         delimited(char('+'), take_until("\r\n"), tag("\r\n")),
-//         std::str::from_utf8,
-//     )
-//     .parse(input)
-// }
 
 fn parse_bulk_string(input: &[u8]) -> IResult<&[u8], &str> {
     let (input, len) = delimited(char('$'), parse_number, tag("\r\n")).parse(input)?;
@@ -131,13 +144,4 @@ fn parse_bulk_string(input: &[u8]) -> IResult<&[u8], &str> {
 fn parse_number(input: &[u8]) -> IResult<&[u8], usize> {
     let digits = map_res(take_while(|b: u8| b.is_ascii_digit()), std::str::from_utf8);
     map_res(digits, str::parse::<usize>).parse(input)
-}
-
-#[test]
-fn parse_array_test() {
-    println!("{:?}", parse_bulk_string(b"$4\r\nECHO\r\n").unwrap());
-    println!(
-        "{:?}",
-        parse_array(b"*2\r\n$4\r\nECHO\r\n$4\r\nPING\r\n").unwrap()
-    );
 }
